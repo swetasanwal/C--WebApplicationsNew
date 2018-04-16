6180199d9fea075ac0621567eec043807724ef5a
Sanwal <ssanwal@amazon.com>
2018-04-15 19:19:42 -0700

    Birthday Card Generator(Assignment 1)

---
+++ BirthdayCardGenerator/BirthdayCardGenerator/Controllers/BirthdayController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCardGenerator.Controllers
+{
+    public class BirthdayController : Controller
+    {
+        // GET: Birthday
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult BirthdayGenerator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult BirthdayGenerator(Models.BirthdayMessageDetails birthdayResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", birthdayResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BirthdayCardGenerator/BirthdayCardGenerator/Models/BirthdayMessageDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayCardGenerator.Models
+{
+    public class BirthdayMessageDetails
+    {
+        [Required(ErrorMessage = "Enter the sender email address")]
+        [EmailAddress]
+        public string Sender { get; set; }
+
+        [Required(ErrorMessage = "Enter the receiver email address")]
+        [EmailAddress]
+        public string Receiver { get; set; }
+
+        [Required(ErrorMessage = "Enter the message")]
+        public string Message { get; set; }
+
+    }
+}
